13a2a6aaec88ef563f89f47f4a86f3e76703870e
Izxchs <ihsanrafeeq007@gmail.com>
2025-11-07 20:11:31 +0530

    Sidebar cooked enough

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Login from "./pages/Login"
 import ForgotPassword from "./pages/ForgotPassword"
 import Register from "./pages/Register"
-// import Home from "./pages/Home"
+import Home from "./pages/Home"
 
 export default function App() {
   return (
@@ -11,7 +11,7 @@ export default function App() {
         <Route path="/" element={<Login />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/register" element={<Register />} /> 
-        {/* <Route path="/home" element={<Home />} /> */}
+        <Route path="/home" element={<Home />} />
       </Routes>
     </BrowserRouter>
   )
